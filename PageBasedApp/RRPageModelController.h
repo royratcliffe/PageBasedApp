484fdf10f6592c39c13415c9ade0395c82cd6dce
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2013-06-07 21:03:34 +0100

    Page model controller with tests

---
+++ PageBasedApp/RRPageModelController.h
@@ -0,0 +1,75 @@
+// RRUIUtils RRPageModelController.h
+//
+// Copyright © 2013, Roy Ratcliffe, Pioneering Software, United Kingdom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+#import <UIKit/UIKit.h>
+
+/**
+ * The page model controller instantiates view controllers for an indexed
+ * collection of abstract page objects. The controller accesses the view
+ * controller implementation's page object property using key-value coding.
+ */
+@interface RRPageModelController : NSObject<UIPageViewControllerDataSource>
+
+@property(copy, nonatomic) NSString *viewControllerIdentifier;
+@property(copy, nonatomic) NSString *viewControllerPageObjectKey;
+
+/**
+ * Answers the index of the given view controller based on its page object's
+ * index within the controller's indexed collection of page objects.
+ */
+- (NSUInteger)indexOfViewController:(UIViewController *)controller;
+
+/**
+ * Instantiates a new view controller for the given index. Sets up the new view
+ * controller with the corresponding page object.
+ *
+ * Turn a page index into a view controller. This requires the identifier of the
+ * view controller and a storyboard which can instantiate the view controller
+ * using the identifier.
+ */
+- (UIViewController *)viewControllerAtIndex:(NSUInteger)index storyboard:(UIStoryboard *)storyboard;
+
+//----------------------------------------------------------------- Page Objects
+
+- (NSUInteger)countOfPageObjects;
+- (id)objectInPageObjectsAtIndex:(NSUInteger)index;
+- (NSArray *)pageObjectsAtIndexes:(NSIndexSet *)indexes;
+- (void)getPageObjects:(__unsafe_unretained id [])buffer range:(NSRange)range;
+
+- (void)insertObject:(id)anObject inPageObjectsAtIndex:(NSUInteger)index;
+- (void)insertPageObjects:(NSArray *)objects atIndexes:(NSIndexSet *)indexes;
+- (void)removeObjectFromPageObjectsAtIndex:(NSUInteger)index;
+- (void)removePageObjectsAtIndexes:(NSIndexSet *)indexes;
+- (void)replaceObjectInPageObjectsAtIndex:(NSUInteger)index withObject:(id)anObject;
+- (void)replacePageObjectsAtIndexes:(NSIndexSet *)indexes withPageObjects:(NSArray *)objects;
+
+@end
+
+/**
+ * Defines the default key applied to the view controller for accessing its page
+ * object. The model controller uses key-value coding to access the view
+ * controller property when instantiating a new view controller and when paging
+ * through view controllers.
+ */
+extern NSString *const kRRPageModelDefaultPageObjectKey;
---
+++ PageBasedApp/RRPageModelController.m
@@ -0,0 +1,150 @@
+// RRUIUtils RRPageModelController.m
+//
+// Copyright © 2013, Roy Ratcliffe, Pioneering Software, United Kingdom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS,” WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
+
+#import "RRPageModelController.h"
+
+NSString *const kRRPageModelDefaultPageObjectKey = @"pageObject";
+
+@interface RRPageModelController()
+
+/**
+ * A mutable array implements the indexed page objects. The page object
+ * interface implements indexed mutable array accessors.
+ */
+@property(strong, nonatomic) NSMutableArray *pageObjects;
+
+- (NSString *)pageObjectKey;
+
+@end
+
+@implementation RRPageModelController
+
+- (id)init
+{
+	self = [super init];
+	if (self)
+	{
+		self.pageObjects = [NSMutableArray array];
+	}
+	return self;
+}
+
+- (NSString *)pageObjectKey
+{
+	return self.viewControllerPageObjectKey ? self.viewControllerPageObjectKey : kRRPageModelDefaultPageObjectKey;
+}
+
+- (NSUInteger)indexOfViewController:(UIViewController *)controller
+{
+	id pageObject = [controller valueForKey:[self pageObjectKey]];
+	return [self.pageObjects indexOfObject:pageObject];
+}
+
+- (UIViewController *)viewControllerAtIndex:(NSUInteger)index storyboard:(UIStoryboard *)storyboard
+{
+	NSUInteger count = [self countOfPageObjects];
+	if (count == 0 || index >= count) return nil;
+	UIViewController *viewController = [storyboard instantiateViewControllerWithIdentifier:self.viewControllerIdentifier];
+	id pageObject = self.pageObjects[index];
+	[viewController setValue:pageObject forKey:[self pageObjectKey]];
+	return viewController;
+}
+
+#pragma mark - Page Objects
+
+// indexed accessors
+
+- (NSUInteger)countOfPageObjects
+{
+	return [self.pageObjects count];
+}
+
+- (id)objectInPageObjectsAtIndex:(NSUInteger)index
+{
+	return [self.pageObjects objectAtIndex:index];
+}
+
+- (NSArray *)pageObjectsAtIndexes:(NSIndexSet *)indexes
+{
+	return [self.pageObjects objectsAtIndexes:indexes];
+}
+
+- (void)getPageObjects:(__unsafe_unretained id [])objects range:(NSRange)range
+{
+	[self.pageObjects getObjects:objects range:range];
+}
+
+// mutable indexed accessors
+
+- (void)insertObject:(id)anObject inPageObjectsAtIndex:(NSUInteger)index
+{
+	[self.pageObjects insertObject:anObject atIndex:index];
+}
+
+- (void)insertPageObjects:(NSArray *)objects atIndexes:(NSIndexSet *)indexes
+{
+	[self.pageObjects insertObjects:objects atIndexes:indexes];
+}
+
+- (void)removeObjectFromPageObjectsAtIndex:(NSUInteger)index
+{
+	[self.pageObjects removeObjectAtIndex:index];
+}
+
+- (void)removePageObjectsAtIndexes:(NSIndexSet *)indexes
+{
+	[self.pageObjects removeObjectsAtIndexes:indexes];
+}
+
+- (void)replaceObjectInPageObjectsAtIndex:(NSUInteger)index withObject:(id)anObject
+{
+	[self.pageObjects replaceObjectAtIndex:index withObject:anObject];
+}
+
+- (void)replacePageObjectsAtIndexes:(NSIndexSet *)indexes withPageObjects:(NSArray *)objects
+{
+	[self.pageObjects replaceObjectsAtIndexes:indexes withObjects:objects];
+}
+
+#pragma mark - Page View Controller Data Source
+
+// The page index does not wrap in either direction: forwards or backwards.
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController
+	  viewControllerBeforeViewController:(UIViewController *)viewController
+{
+	NSUInteger index = [self indexOfViewController:viewController];
+	if (index == NSNotFound || index == 0) return nil;
+	return [self viewControllerAtIndex:index - 1 storyboard:viewController.storyboard];
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController
+	   viewControllerAfterViewController:(UIViewController *)viewController
+{
+	NSUInteger index = [self indexOfViewController:viewController];
+	if (index == NSNotFound || index + 1 == [self countOfPageObjects]) return nil;
+	return [self viewControllerAtIndex:index + 1 storyboard:viewController.storyboard];
+}
+
+@end
