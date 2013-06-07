aa96899aae0062f3b4ed3cae1e235a8f0511b031
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2013-06-07 21:19:24 +0100

    Remove testing stubs

--- PageBasedAppTests/PageBasedAppTests.m
+++ PageBasedAppTests/PageBasedAppTests.m
@@ -10,23 +10,4 @@
 
 @implementation PageBasedAppTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in PageBasedAppTests");
-}
-
 @end
